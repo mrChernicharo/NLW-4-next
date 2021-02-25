04da03131c57afd29ec0cc8c97d755c39ac8cce6
Felipe Chernicharo <string7dev@gmail.com>
2021-02-25 17:10:18 -0300

    assets (/public)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/Profile.module.css';
+
 const Profile = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <img
         src="https://avatars.githubusercontent.com/u/52944886?s=400&u=848be9ef8d675be4453d7254cdeae48bfcfe848a&v=4"
         alt="Avatar usuário"
