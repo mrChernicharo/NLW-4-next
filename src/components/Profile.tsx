e6daff083d6ade2c5c0146dbcfdb47d5256d5b2f
Felipe Chernicharo <string7dev@gmail.com>
2021-02-25 16:59:57 -0300

    iniciando profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+const Profile = () => {
+  return (
+    <div>
+      <img
+        src="https://avatars.githubusercontent.com/u/52944886?s=400&u=848be9ef8d675be4453d7254cdeae48bfcfe848a&v=4"
+        alt="Avatar usuário"
+      />
+      <div>
+        <strong>Felipe Chernicharo</strong>
+        <p>level 1</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
