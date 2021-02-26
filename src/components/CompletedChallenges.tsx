122bf2a55934088098ec5287902b6a5fa0fdce61
Felipe Chernicharo <string7dev@gmail.com>
2021-02-25 21:36:28 -0300

    completedChallenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/CompletedChallenges.module.css';
+const CompletedChallenges = () => {
+  return (
+    <div className={styles.container}>
+      <span>Desafios Completados</span>
+      <span>5</span>
+    </div>
+  );
+};
+
+export default CompletedChallenges;
