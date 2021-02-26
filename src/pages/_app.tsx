0eca54e63c090968efbcf4ee1d2a07f39b91ec17
Felipe Chernicharo <string7dev@gmail.com>
2021-02-26 00:29:12 -0300

    iniciando com context API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import { challengeContext } from '../contexts/ChallengeContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
