f254af7166abcbc9522913ff371c1873e005555d
Felipe Chernicharo <string7dev@gmail.com>
2021-02-26 16:47:49 -0300

    bugfix ->  uso correto dos contextos. aninhamento

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 
 import { ChallengesProvider } from '../contexts/ChallengeContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,3 +12,14 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// é comum a gente ter que disponibilizar vários contextos
+// isso é possível
+// só se liga que as vezes um contexto depende de outro
+// Aí o que provê as dependências deve envolver o dependente, assim:
+
+// <ChallengesProvider>
+//    <CountdownProvider>
+//        <Component {...pageProps} />
+//    </CountdownProvider>
+// </ChallengesProvider>
