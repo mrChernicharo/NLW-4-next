e07767671a4b2dfb23326765b7135cc7f6dd6e65
Felipe Chernicharo <string7dev@gmail.com>
2021-02-25 22:42:24 -0300

    minutes e seconds

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
+
 export default function Countdown() {
+  // em Segundos
+  const [time, setTime] = useState(25 * 60);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
   return (
     <div>
       <div className={styles.container}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
       <button type="button" className={styles.countdownBtn}>
