1abaafb5171b9f5eb4f8ded00770ae6894ff27b6
Felipe Chernicharo <string7dev@gmail.com>
2021-02-26 00:41:23 -0300

    passa funçoes pelo contexto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css';
 
 import { ChallengeContext } from '../contexts/ChallengeContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [level, setLevel] = useState(1);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
   return (
-    <ChallengeContext.Provider value={{ level: 1 }}>
+    <ChallengeContext.Provider value={{ level: 1, levelUp }}>
       <Component {...pageProps} />
     </ChallengeContext.Provider>
   );
